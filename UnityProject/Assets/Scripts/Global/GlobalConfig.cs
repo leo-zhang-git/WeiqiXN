5a4abf2e4cb84165b08dd3aa687c0680c95f823e
LinXinnan <xinnansama@163.com>
2026-05-03 22:27:42 +0800

    Config data type get jobject
    Config data type get jobject

--- UnityProject/Assets/Scripts/Global/GlobalConfig.cs
+++ UnityProject/Assets/Scripts/Global/GlobalConfig.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json.Linq;
+using System.IO;
 using UnityEngine;
 
 public static class GlobalConfig
@@ -5,7 +7,21 @@ public static class GlobalConfig
     public static string INGAME_DEBUG_CONSOLE_PREFAB_CONFIG_ID = "IngameDebugConsole";
     public readonly static string PATH_START_SCENE = "Assets/Scenes/Main.unity";
 #if UNITY_EDITOR
-    public readonly static string PATH_CONFIG_JSON = Application.dataPath + "/Config/DataJson";
+    private readonly static string PATH_CONFIG_JSON = Application.dataPath + "/Config/DataJson";
+    public static JObject GetJsonConfigJObject(string jsonConfigName)
+    {
+        string jsonPath = Path.Combine(GlobalConfig.PATH_CONFIG_JSON, jsonConfigName, jsonConfigName + ".json");
+        if (File.Exists(jsonPath)) {
+            return JObject.Parse(File.ReadAllText(jsonPath));
+        } else {
+            return null;
+        }
+    }
+#else
+    public static JObject GetJsonConfigJObject(string jsonConfigName)
+    {
+        return null;
+    }
 #endif
 
     public static bool ENABLE_SIMULATE_LOAD_RESOURCE_SYNC = true;
