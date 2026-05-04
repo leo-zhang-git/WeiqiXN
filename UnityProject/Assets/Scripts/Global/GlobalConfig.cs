bb9eed0fac413dc537d00e7a1389397bcc1795da
LinXinnan <xinnansama@163.com>
2026-05-04 23:57:51 +0800

    GlobalConfig get json config from bundle
    GlobalConfig get json config from bundle

--- UnityProject/Assets/Scripts/Global/GlobalConfig.cs
+++ UnityProject/Assets/Scripts/Global/GlobalConfig.cs
@@ -7,8 +7,10 @@ public static class GlobalConfig
     public static string INGAME_DEBUG_CONSOLE_PREFAB_CONFIG_ID = "IngameDebugConsole";
     public readonly static string PATH_START_SCENE = "Assets/Scenes/Main.unity";
     public readonly static string PATH_ASSET_BUNDLE = Application.streamingAssetsPath + "/AssetBundles";
-#if UNITY_EDITOR
+    public const string CONFIG_JSON_BUNDLE_NAME = "config_json";
     private readonly static string PATH_CONFIG_JSON = Application.dataPath + "/Config/DataJson";
+
+#if UNITY_EDITOR
     public static JObject GetJsonConfigJObject(string jsonConfigName)
     {
         string jsonPath = Path.Combine(GlobalConfig.PATH_CONFIG_JSON, jsonConfigName, jsonConfigName + ".json");
@@ -21,6 +23,17 @@ public static class GlobalConfig
 #else
     public static JObject GetJsonConfigJObject(string jsonConfigName)
     {
+        if (Global.Instance.resourceManager.bundleDict.TryGetValue(CONFIG_JSON_BUNDLE_NAME, out var bundle)) {
+            string jsonPath = $"Assets/Config/DataJson/{jsonConfigName}/{jsonConfigName}.json";
+            TextAsset jsonAsset = bundle.LoadAsset<TextAsset>(jsonPath);
+            if (jsonAsset != null) {
+                return JObject.Parse(jsonAsset.text);
+            }
+            XNLogger.LogError("Json config asset not found in asset bundle.", ("jsonConfigName", jsonConfigName));
+        } else {
+            XNLogger.LogError("Json config bundle not found.", ("bundleName", CONFIG_JSON_BUNDLE_NAME));
+        }
+
         return null;
     }
 #endif
